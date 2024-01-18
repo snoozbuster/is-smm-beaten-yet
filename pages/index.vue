9e91e6496b5435bf10fa397ef37a31c2a1e8387e
snoozbuster <snoozbuster@outlook.com>
2024-01-18 00:25:35 -0800

    minor fixes + refactoring for stats feature

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,11 @@
 <template>
-  <main
-    class="text-center h-screen grid place-content-center bg-smm-yellow uppercase overflow-hidden"
-  >
-    <h1 id="the-answer" :class="!animationStarted && 'hidden'">Not yet</h1>
+  <main class="h-screen" :class="'overflow-hidden'">
+    <TheAnswer class="h-screen" />
     <SocialLinks />
   </main>
 </template>
 
-<style lang="scss" scoped>
-#the-answer {
-  font-family: 'Super Mario Maker';
-  font-size: clamp(10rem, 20vw, 20rem);
-  user-select: none;
-  clip-path: polygon(0 0, 100% 1%, 100% 100%, 0% 100%);
-}
-
-:deep(.char) {
-  opacity: 0;
-  transform: translateY(100%);
-  transition: transform, opacity;
-}
-</style>
-
 <script setup lang="ts">
-import SplitType from 'split-type';
-import gsap from 'gsap';
-
-const animationStarted = ref(false);
-
 useSeoMeta({
   title: 'Is Super Mario Maker Beaten Yet?',
   ogTitle: 'Is Super Mario Maker Beaten Yet?',
