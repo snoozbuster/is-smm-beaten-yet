2c90e85ff183a60a7a85c762c151543e13c47871
snoozbuster <snoozbuster@outlook.com>
2024-01-18 20:53:40 -0800

    sync hash with viewed panel

--- pages/index.vue
+++ pages/index.vue
@@ -89,6 +89,7 @@ onMounted(() => {
     observer = new IntersectionObserver(
       (entries) => {
         scrolled.value = entries.some((entry) => entry.isIntersecting);
+        window.location.hash = scrolled.value ? 'stats' : '';
       },
       {
         root: document.getElementsByTagName('main')[0],
