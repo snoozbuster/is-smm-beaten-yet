9e096cc351e0d4e09795faa337b4be3cbe06fc7d
snoozbuster <snoozbuster@outlook.com>
2024-01-24 00:26:01 -0800

    add lambda function to sync gsheet data to s3

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,12 @@
+export type AmplifyDependentResourcesAttributes = {
+  "function": {
+    "SyncLevelsGSheets": {
+      "Arn": "string",
+      "CloudWatchEventRule": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
