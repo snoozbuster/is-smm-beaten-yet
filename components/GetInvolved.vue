e61c77dfead472934b794011f25790b09b623608
snoozbuster <snoozbuster@outlook.com>
2024-01-24 01:19:23 -0800

    text

--- components/GetInvolved.vue
+++ components/GetInvolved.vue
@@ -3,7 +3,7 @@
     <h4 class="text-2xl font-semibold mb-5">Help us out!</h4>
     <p class="text-lg mb-3">
       All you have to do to be a part of Team 0% is to clear one uncleared
-      level.
+      level. Join the discord to learn more and view the full list of levels!
     </p>
     <h4 class="text-xl font-semibold mb-3">Community links</h4>
     <SocialLinks />
