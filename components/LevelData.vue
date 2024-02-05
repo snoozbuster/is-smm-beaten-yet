695ca2af2a12e1961c9569838d3f3d4f6b5a57ed
snoozbuster <snoozbuster@outlook.com>
2024-02-05 02:14:50 -0800

    make first scroll on mobile smoother?

--- components/LevelData.vue
+++ components/LevelData.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="bg-course-world text-course-world-contrast md:h-screen">
+  <div
+    class="bg-course-world text-course-world-contrast min-h-screen md:h-screen"
+  >
     <div
       class="grid p-7 grid-flow-row grid-rows-1 md:grid-rows-2 grid-cols-1 md:grid-cols-2 xl:grid-cols-3 w-full h-full gap-7 overflow-hidden"
       :class="!animationStarted && 'invisible'"
