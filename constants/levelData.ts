12d83236413a7c18c779bb3716e087c3403a935c
snoozbuster <snoozbuster@outlook.com>
2024-03-06 01:57:51 -0800

    start using cdn again

--- constants/levelData.ts
+++ constants/levelData.ts
@@ -1,5 +1,4 @@
-export const DATA_ROOT_URL =
-  'https://is-smm-beaten-yet-public-data.s3.us-west-2.amazonaws.com/levels';
+export const DATA_ROOT_URL = 'https://static.issmmbeatenyet.com/levels';
 
 export const COUNTRIES = [
   { value: 'JP', name: 'Japan' },
