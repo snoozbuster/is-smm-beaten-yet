a88fa82ed8b13b7d3d61016cf20a1802a648d656
snoozbuster <snoozbuster@outlook.com>
2024-03-18 01:42:41 -0700

    update lambda to parse alarm message

--- amplify/backend/function/DiscordErrorNotifier/src/index.js
+++ amplify/backend/function/DiscordErrorNotifier/src/index.js
@@ -13,20 +13,29 @@ exports.handler = async (event) => {
   console.log(`EVENT: ${JSON.stringify(event)}`);
   console.log('WEBHOOK URL:', process.env.DiscordWebhookUrl);
 
-  const res = await axios.post(process.env.DiscordWebhookUrl, {
-    content: 'Hello from AWS!',
-    username: 'issmmbeatenyet.com',
-  });
+  try {
+    const isOk = event.alarmData.state.value === 'OK';
+    let message = `${event.alarmData.alarmName} has changed state to ${event.alarmData.state.value}. `;
+    message += isOk
+      ? `The issue has been resolved.`
+      : `Something is going wrong.`;
+    message += `\n\nThe reason for this transition was: ${event.alarmData.state.reason}`;
 
-  console.log('Response from discord:', res);
+    const res = await axios.post(process.env.DiscordWebhookUrl, {
+      content: message,
+      username: 'issmmbeatenyet.com Monitor',
+    });
+    console.log('Response from discord:', res);
+  } catch (e) {
+    const res = await axios.post(process.env.DiscordWebhookUrl, {
+      content: `Error while parsing alarm and generating Discord message: ${e.message}`,
+      username: 'issmmbeatenyet.com Monitor',
+    });
+    console.log('Response from discord:', res);
+  }
 
   return {
     statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  },
     body: JSON.stringify('Hello from Lambda!'),
   };
 };
