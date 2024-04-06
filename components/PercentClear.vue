5622b87b60b501130a33bd4e09b049d6238ba097
snoozbuster <snoozbuster@outlook.com>
2024-04-05 19:59:55 -0700

    update site with TTH news

--- components/PercentClear.vue
+++ components/PercentClear.vue
@@ -1,7 +1,65 @@
 <template>
   <div class="align-center flex h-full justify-center position-relative w-full">
     <Doughnut :data="data" :options="options" />
+    <PrimeButton
+      class="hover:underline text-course-world-contrast absolute block mx-auto p-0 text-sm font-normal top-1/2 left-1/2 -translate-x-1/2 mt-5"
+      link
+      @click="modalOpen = true"
+    >
+      Learn why
+      <i class="pi pi-angle-right -ml-1 text-sm"></i>
+    </PrimeButton>
   </div>
+  <PrimeDialog
+    v-model:visible="modalOpen"
+    class="w-full md:w-2/3"
+    header="101% Clear"
+    modal
+    dismissable-mask
+  >
+    <p class="mb-3">
+      Originally, it was believed that a level called Trimming the Herbs would
+      be the final uncleared level. However, after over seven years, the creator
+      of Trimming the Herbs revealed that the level was, in fact, a convoluted
+      sort of troll. Despite a general belief both then and now that
+      tool-assisted speedruns (TASes) were not possible on the Wii U, the
+      creator had access to a hardware-based TAS prototype which was used to
+      develop and clear-check TTH. It was apparently intended to be revealed to
+      the public as such, but the level remained relatively obscure even in the
+      community it was designed to troll and was eventually forgotten (until
+      Team 0% uncovered it). Since the level was not uploaded legitimately, it
+      was excluded from Team 0%'s list of uncleared levels. As a result of this
+      shocking revelation, Team 0% considers Super Mario Maker 1 to have been
+      100% cleared on {{ formatDate('2024-03-15') }}, the day that Yamada (aka
+      "kazeihinn") cleared The Last Dance.
+    </p>
+
+    <p class="mb-3">
+      However, this isn't quite the end of the story. Trimming the Herbs isn't
+      <i>impossible</i> for a human, and on
+      {{ formatDate('2024-04-05T5:15:00-07:00') }} (only a couple days before
+      the SMM1 servers shut down for good), after over 100 hours of practice and
+      attempts, a player by the name of
+      <a
+        class="text-blue-500 hover:underline"
+        href="https://twitter.com/sanyx91smm2"
+        target="_blank"
+        >Sanyx91SMM2</a
+      >
+      did what the original creator could not: he achieved the
+      <a
+        class="text-blue-500 hover:underline"
+        href="https://twitter.com/sanyx91smm2/status/1776407036160291101"
+        target="_blank"
+        >first true clear</a
+      >
+      of TTH. Since TTH was excluded from the team's goal due to the nature of
+      its original upload and 100% was already achieved, we choose to treat this
+      clear of the "secret final boss" of Super Mario Maker as the 101st
+      completion percentage to signify his dedication to going above and beyond
+      in the effort to truly complete every level in the game.
+    </p>
+  </PrimeDialog>
 </template>
 
 <script setup lang="ts">
@@ -27,23 +85,17 @@ const props = defineProps({
   },
 });
 
-const { formatPercent } = useFormatters();
-const { theAnswer } = await useTheAnswer();
+const modalOpen = ref(false);
+
+const { formatPercent, formatDate } = useFormatters();
 
 const unclearedLevels = computed(() => {
-  return props.unclearedLevels === 0 && theAnswer.value === 'Not yet'
-    ? 1
-    : props.unclearedLevels;
+  return props.unclearedLevels;
 });
 
 const percentClear = computed(() => {
-  const numerator = props.clearedLevels;
-  const denominator = props.clearedLevels + unclearedLevels.value;
-
-  const precision = numerator / denominator >= 0.991 ? 2 : 0;
-
-  return formatPercent(numerator, denominator, {
-    precision,
+  return formatPercent(101, 100, {
+    precision: 0,
     rounding: 'trunc',
   });
 });
