361eedb86a21826069f73dae56ca2b34b9412a70
snoozbuster <snoozbuster@outlook.com>
2025-03-02 20:00:07 -0800

    better eslint config

--- amplify/backend/function/SyncLevelsGSheets/src/.eslintrc.cjs
+++ amplify/backend/function/SyncLevelsGSheets/src/.eslintrc.cjs
@@ -1,4 +1,14 @@
 module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+    browser: false,
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   rules: {
     '@typescript-eslint/no-var-requires': 0,
     'no-console': 0,
