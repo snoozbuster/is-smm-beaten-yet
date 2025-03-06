f495af621a5be5ad20028a2348c938d5d3779bfc
snoozbuster <snoozbuster@outlook.com>
2025-03-06 00:38:09 -0800

    use actual country name in flag alt text + tooltip

--- components/CountryFlag.vue
+++ components/CountryFlag.vue
@@ -1,26 +1,28 @@
 <template>
   <img
-    :class="[
-      'inline',
-      'border',
-      'border-black/25',
-      'h-[19px]',
-      'align-text-bottom',
-    ]"
+    v-tooltip.hover="countryName"
+    :class="['inline', 'border', 'border-black/25', 'h-[19px]', 'align-middle']"
     :src="`https://flagcdn.com/h20/${countryCode.toLowerCase()}.png`"
     :srcset="`
       https://flagcdn.com/h40/${countryCode.toLowerCase()}.png 2x,
       https://flagcdn.com/h60/${countryCode.toLowerCase()}.png 3x
     `"
-    alt="Ukraine"
+    :alt="countryName"
   />
 </template>
 
 <script setup lang="ts">
-defineProps({
+const props = defineProps({
   countryCode: {
     type: String,
     required: true,
   },
 });
+
+const regionNames = new Intl.DisplayNames(navigator.languages, {
+  type: 'region',
+});
+const countryName = computed(() =>
+  regionNames.of(props.countryCode.toUpperCase()),
+);
 </script>
