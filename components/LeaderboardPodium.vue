b495cd0a99d151461ac8529f40449d318112ae44
snoozbuster <snoozbuster@outlook.com>
2025-03-30 12:47:26 -0700

    I think this podium UI is better

--- components/LeaderboardPodium.vue
+++ components/LeaderboardPodium.vue
@@ -1,37 +1,16 @@
 <template>
   <CourseWorldCard class="w-fit" :grid="false">
     <h3 class="text-xl">{{ name }}</h3>
-    <div>
+    <div class="flex flex-row-reverse flex-wrap justify-center mb-8 gap-4">
       <svg
-        width="400"
-        height="200"
-        viewBox="0 0 200 200"
-        preserve-aspect-ratio="xMidYMid meet"
+        width="200"
+        height="150"
+        viewBox="0 0 200 150"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <!-- Base background -->
-        <!-- <rect width="200" height="200" fill="#f5f5f5" /> -->
-
-        <!-- Podium: 1st place -->
-        <foreignObject x="30" y="25" width="200" height="22.5">
-          <div xmlns="http://www.w3.org/1999/xhtml">
-            <PlayerNnid :nnid="first?.nnid" :size="20" :awards="false" />
-          </div>
-        </foreignObject>
-        <rect x="75" y="50" width="50" height="100" fill="#DAA520" />
         <text
           x="100"
-          y="70"
-          font-family="Arial"
-          font-size="16"
-          text-anchor="middle"
-          fill="#000"
-        >
-          {{ formatNumber(first?.score) }}
-        </text>
-        <text
-          x="100"
-          y="175"
+          y="40"
           font-family="Arial"
           font-size="20"
           text-anchor="middle"
@@ -39,30 +18,23 @@
         >
           {{ LEADERBOARD_RANK_MEDALS[1] }}
         </text>
-
-        <!-- Podium: 2nd place -->
-        <foreignObject x="-135" y="53" width="200" height="22.5">
-          <div
-            xmlns="http://www.w3.org/1999/xhtml"
-            class="text-right flex flex-row-reverse"
-          >
-            <PlayerNnid :nnid="second?.nnid" :size="20" :awards="false" />
-          </div>
-        </foreignObject>
-        <rect x="25" y="75" width="50" height="75" fill="#d0d0d0" />
+        <rect x="75" y="50" width="50" height="100" fill="#DAA520" />
         <text
-          x="50"
-          y="95"
+          x="100"
+          y="70"
           font-family="Arial"
           font-size="16"
           text-anchor="middle"
+          font-weight="700"
           fill="#000"
         >
-          {{ formatNumber(second?.score) }}
+          {{ formatNumber(first?.score) }}
         </text>
+
+        <!-- Podium: 2nd place -->
         <text
           x="50"
-          y="175"
+          y="65"
           font-family="Arial"
           font-size="20"
           text-anchor="middle"
@@ -70,27 +42,23 @@
         >
           {{ LEADERBOARD_RANK_MEDALS[2] }}
         </text>
-
-        <!-- Podium: 3rd place -->
-        <foreignObject x="90" y="75" width="200" height="22.5">
-          <div xmlns="http://www.w3.org/1999/xhtml">
-            <PlayerNnid :nnid="third?.nnid" :size="20" :awards="false" />
-          </div>
-        </foreignObject>
-        <rect x="125" y="100" width="50" height="50" fill="#A56F2B" />
+        <rect x="25" y="75" width="50" height="75" fill="#d0d0d0" />
         <text
-          x="150"
-          y="120"
+          x="50"
+          y="95"
           font-family="Arial"
           font-size="16"
           text-anchor="middle"
+          font-weight="700"
           fill="#000"
         >
-          {{ formatNumber(third?.score) }}
+          {{ formatNumber(second?.score) }}
         </text>
+
+        <!-- Podium: 3rd place -->
         <text
           x="150"
-          y="175"
+          y="90"
           font-family="Arial"
           font-size="20"
           text-anchor="middle"
@@ -98,10 +66,34 @@
         >
           {{ LEADERBOARD_RANK_MEDALS[3] }}
         </text>
+        <rect x="125" y="100" width="50" height="50" fill="#A56F2B" />
+        <text
+          x="150"
+          y="120"
+          font-family="Arial"
+          font-size="16"
+          text-anchor="middle"
+          font-weight="700"
+          fill="#000"
+        >
+          {{ formatNumber(third?.score) }}
+        </text>
       </svg>
+      <div class="text-left self-center sm:mb-0">
+        <div
+          v-for="i in 3"
+          :key="leaderboard[i - 1]?.nnid"
+          class="flex items-center text-lg"
+        >
+          <span class="text-xl mr-1">
+            {{ LEADERBOARD_RANK_MEDALS[i] }}
+          </span>
+          <PlayerNnid :nnid="leaderboard[i - 1]?.nnid" :size="20" />
+        </div>
+      </div>
     </div>
     <div class="pt-2 border-t -mx-3 px-3 -mb-2">
-      <button class="group-hover:underline text-nowrap" @click="viewing = true">
+      <button class="hover:underline text-nowrap" @click="viewing = true">
         View full <Icon name="mdi:chevron-right" />
       </button>
 
